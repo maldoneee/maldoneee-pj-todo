f94fd6b967a94c695557f60d59871536c62d9d2e
maldoneee <maldoneee@gmail.com>
2024-11-11 21:34:25 +0900

    feat: 상태 초기화 및 필터링 로직 개선
    - 필터링 로직을 useTodoStore로 이동하여 컴포넌트와 로직 분리
      - todos, activeFilter, priorityFilter, searchFilterQuery 상태를 기반으로 필터링된 todos를 반환하는 selector 추가
    - 상태 초기화를 위한 initialState와 reset 메서드 구현
      - 상태 초기화 로직을 한 곳에서 관리하여 유지보수성을 향상
    - filteredTodos를 TodoList 컴포넌트에서 직접 가져오도록 수정하여 코드 가독성 및 재사용성 개선

--- src/components/todos/TodoList.jsx
+++ src/components/todos/TodoList.jsx
@@ -1,34 +1,11 @@
 import {TodoItem} from './index.js';
 import useTodoStore from '../../store/todoStore.js';
-import {useMemo} from 'react';
 
 const TodoList = () => {
 
   const {
-    todos,
-    activeFilter,
-    priorityFilter,
-    searchFilterQuery,
+    filteredTodos,
   } = useTodoStore();
-
-  const filteredTodos = useMemo(() =>
-          todos.filter((todo) => searchFilterQuery === '' ? todo :
-              todo.text.toLowerCase().includes(searchFilterQuery.toLowerCase()))
-               .filter((todo) => priorityFilter ===
-                   'all' || todo.priority ===
-                   priorityFilter)
-               .filter((todo) => {
-                     switch (activeFilter) {
-                       case 'completed':
-                         return todo.completed;
-                       case 'incomplete':
-                         return !todo.completed;
-                       default: //all
-                         return true;
-                     }
-                   },
-               ),
-      [todos, searchFilterQuery, activeFilter, priorityFilter]);
 
   return (
       <ul className="todo-list">
--- src/store/todoStore.js
+++ src/store/todoStore.js
@@ -1,10 +1,41 @@
 import {create} from 'zustand';
 
-const useTodoStore = create((set) => ({
+const initialState = {
   todos: [],
   activeFilter: 'all',
   priorityFilter: 'all',
   searchFilterQuery: '',
+};
+
+const useTodoStore = create((set, get) => ({
+  ...initialState,
+
+  reset: () => set(initialState),
+
+  filteredTodos: () => {
+    const {todos, activeFilter, priorityFilter, searchFilterQuery} = get();
+    return todos
+        .filter((todo) =>
+            searchFilterQuery === ''
+                ?
+                true
+                :
+                todo.text.toLowerCase()
+                    .includes(searchFilterQuery.toLowerCase()),
+        )
+        .filter((todo) => priorityFilter === 'all' || todo.priority ===
+            priorityFilter)
+        .filter((todo) => {
+          switch (activeFilter) {
+            case 'completed':
+              return todo.completed;
+            case 'incomplete':
+              return !todo.completed;
+            default:
+              return true;
+          }
+        });
+  },
 
   addTodo: ((text, priority) =>
       set((state) => ({
